5751d3d65bd66a008dc62fefcb25691650df77fa
unknown <24abdullahalmasum@gmail.com>
2023-06-10 00:18:03 +0600

    added admin system

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(express.json());
@@ -13,32 +13,32 @@ app.use(cors());
 app.use(morgan('dev'));
 
 // jwt
-const verifyJWT = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error: true, message: 'unauthorized access'})
-  }
-  const token = authorization.split(' ')[1]
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-    if(err){
-      return res.status(401).send({error: true, message: 'unauthorized access'})
-    }
-    req.decoded = decoded
-    next()
-  })
-  
-}
+// const verifyJWT = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   if (!authorization) {
+//     return res.status(401).send({ error: true, message: 'unauthorized access' })
+//   }
+//   const token = authorization.split(' ')[1]
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ error: true, message: 'unauthorized access' })
+//     }
+//     req.decoded = decoded
+//     next()
+//   })
 
-app.post('/jwt', async(req, res) => {
-  const email = req.body
- 
-  const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { 
-    expiresIn: '7d'
-  })
+// }
 
- 
-  res.send({token})
-})
+// app.post('/jwt', async (req, res) => {
+//   const email = req.body
+
+//   const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+//     expiresIn: '7d'
+//   })
+
+
+//   res.send({ token })
+// })
 
 
 // mongodb
@@ -63,61 +63,88 @@ async function run() {
 
 
     // save user email and role
-  app.put('/users/:email', async (req, res) =>{
-    const email = req.params.email
-    const user = req.body
-    const query = { email: email }
-    const options = { upsert: true }
-    const UpdateDoc = {
-      $set: user,
-    }
-    const result = await userCollection.updateOne(query,UpdateDoc,options)
-    console.log(result);
-    res.send(result)
-  })
-
-  // get all users
-  app.get('/users', async (req, res) =>{
-    const result = await userCollection.find().toArray()
-    res.send(result)
-  })
-
-  // get admin
-  app.get('/users/admin/:email', async (req, res) => {
-    const email = req.params.email
-
-    const query = { email: email }
-    const user = await userCollection.findOne(query)
-    const result = { admin: user?.role === 'Admin'}
-    res.send(result)
-  })
-
-  app.patch('/users/admin/:id', async (req, res) => {
-    const id = req.params.id;
-    const role = req.body
-    const filter = {_id: new ObjectId(id)}
-    const updateDoc = {
-      $set: {
-        role: role
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email: email }
+      const options = { upsert: true }
+      const UpdateDoc = {
+        $set: user,
       }
-    }
-    const result = await usersCollection.updateOne(filter,updateDoc)
-    res.send(result)
-  })
+      const result = await userCollection.updateOne(query, UpdateDoc, options)
+      console.log(result);
+      res.send(result)
+    })
+
+    // get all users
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    // get admin
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      const result = { admin: user?.role === 'Admin' }
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: "Admin"
+        }
+      }
+      const result = await userCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: "Instructor"
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    app.patch('/users/student/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: "Student"
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    // app.get('/users/:id', async (req, res) => {
+    //   const id = req.params.id
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await userCollection.findOne(query)
+    //   res.send(result)
+    // })
 
 
 
-  // post class
-  app.post('/classes',  async (req, res) => {
-    const newItem = req.body
-    const result = await classCollection.insertOne(newItem)
-    res.send(result)
-  })
+    // post class
+    app.post('/classes', async (req, res) => {
+      const newItem = req.body
+      const result = await classCollection.insertOne(newItem)
+      res.send(result)
+    })
 
-  app.get('/classes', async (req, res) => {
-    result = await classCollection.find().toArray();
-    res.send(result)
-  })
+    app.get('/classes', async (req, res) => {
+      result = await classCollection.find().toArray();
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -132,9 +159,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('school is running');
-  })
-  
-  app.listen(port, () => {
-    console.log(`school is running on port${port}`)
-  })
+  res.send('school is running');
+})
+
+app.listen(port, () => {
+  console.log(`school is running on port${port}`)
+})

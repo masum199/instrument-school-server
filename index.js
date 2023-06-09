8e623c1d3377d173a06f23aa6f8ccaf2c1f564e0
unknown <24abdullahalmasum@gmail.com>
2023-06-09 18:52:30 +0600

    added jwt post

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -12,20 +13,31 @@ app.use(cors());
 app.use(morgan('dev'));
 
 // jwt
-// const verifyJWT = (req, res, next) => {
-//   const authorization = req.headers.authorization;
-//   if(!authorization){
-//     return res.status(401).send({error: true, message: 'unauthorized access'})
-//   }
-//   const token = authorization.split(' ')[1]
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-//     if(err){
-//       return res.status(401).send({error: true, message: 'unauthorized access'})
-//     }
-//     req.decoded = decoded
-//     next()
-//   })
-// }
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'unauthorized access'})
+  }
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
+app.post('/jwt', async(req, res) => {
+  const email = req.body
+  console.log(email)
+  const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { 
+    expiresIn: '1h'
+  })
+
+  console.log(token)
+  res.send({token})
+})
 
 
 // mongodb
